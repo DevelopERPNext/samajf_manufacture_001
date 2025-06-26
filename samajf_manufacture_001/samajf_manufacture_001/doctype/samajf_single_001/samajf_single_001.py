65d2f6ba187b70dad3693ba1968f7a127f029784
DevelopERPNext <develop.erpnext@gmail.com>
2025-06-26 17:01:21 +0300

    commit 3

---
+++ samajf_manufacture_001/samajf_manufacture_001/doctype/samajf_single_001/samajf_single_001.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, khattab@info.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class samajf_single_001(Document):
+	pass
